f605f91f6120138bf6b0d7826a62d5b93e2118b7
atsushi otani <atsushi.ootani@gmail.com>
2020-01-20 17:50:14 +0900

    盤面表示機能

--- reversi.go
+++ reversi.go
@@ -2,23 +2,41 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 const FieldWidth = 8
 const FieldHeight = 8
 
+const VOID = 0
+const BLACK = 1
+const WHITE = 2
+
 var fields [FieldWidth][FieldHeight]int
 
 func main() {
-	fmt.Println("aaa")
-	printFields(fields)
+	fields[3][3] = BLACK
+	fields[4][4] = BLACK
+	fields[3][4] = WHITE
+	fields[4][3] = WHITE
+ 	printFields(fields)
 }
 
 func printFields(fields [FieldWidth][FieldHeight]int) {
-	fmt.Print("-")
 	for x := 0; x < FieldWidth; x++ {
+		fmt.Println(strings.Repeat("-", FieldWidth * 4 + 1))
 		for y := 0; y < FieldHeight; y++ {
-			fmt.Print(fields[x][y])
+			fmt.Print("|")
+			mark := "   "
+			if fields[x][y] == BLACK {
+				mark = " ● "
+			} else if fields[x][y] == WHITE {
+				mark = " ○ "
+			}
+
+			fmt.Print(mark)
 		}
+		fmt.Println("|")
 	}
+	fmt.Println(strings.Repeat("-", FieldWidth * 4 + 1))
 }
