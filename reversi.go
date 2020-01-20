5cd446efd9f4219c71936b004561bc42ad556521
atsushi otani <atsushi.ootani@gmail.com>
2020-01-20 17:27:23 +0900

    simple print

--- reversi.go
+++ reversi.go
@@ -1,9 +1,24 @@
 package main
 
-import(
+import (
 	"fmt"
 )
 
-func main(){
-  fmt.Println("aaa")
+const FieldWidth = 8
+const FieldHeight = 8
+
+var fields [FieldWidth][FieldHeight]int
+
+func main() {
+	fmt.Println("aaa")
+	printFields(fields)
+}
+
+func printFields(fields [FieldWidth][FieldHeight]int) {
+	fmt.Print("-")
+	for x := 0; x < FieldWidth; x++ {
+		for y := 0; y < FieldHeight; y++ {
+			fmt.Print(fields[x][y])
+		}
+	}
 }
